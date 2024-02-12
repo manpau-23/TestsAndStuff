4217ea7ece0ac3cacc67be92e3b5daed4632913e
manpau <manpaupc23@gmail.com>
2024-02-12 15:46:53 -0500

    Express module uses src/public/index.html as webpage homepage.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const Port = process.env.PORT;
 
-app.use(express.static('public'));
+const staticPath = path.join(__dirname, 'public');
+app.use(express.static(staticPath));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(staticPath, 'index.html'));
+});
+  
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
